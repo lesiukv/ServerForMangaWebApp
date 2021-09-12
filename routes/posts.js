8f6ba024fbb554da939af79d9c09e76dba422631
Volodymyr <lesiyk47@gmail.com>
2021-09-12 22:36:37 +0300

    formated project code

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import { getPosts, createPost, getPostDetails, deletePost, updatePost } from '../controllers/posts.js'
+import express from "express";
+import {
+  getPosts,
+  createPost,
+  getPostDetails,
+  deletePost,
+  updatePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get('/', getPosts);
-router.post('/', createPost);
-router.get('/:id', getPostDetails);
-router.delete('/:id', deletePost);
-router.patch('/:id', updatePost);
+router.get("/", getPosts);
+router.post("/", createPost);
+router.get("/:id", getPostDetails);
+router.delete("/:id", deletePost);
+router.patch("/:id", updatePost);
 
 export default router;
