bfd1b6266d41958c4dd03e1806d9fe83547d6f93
Volodymyr <lesiyk47@gmail.com>
2021-09-12 15:27:43 +0300

    added update and delete

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getPosts, createPost, getPostDetails } from '../controllers/posts.js'
+import { getPosts, createPost, getPostDetails, deletePost, updatePost } from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.get('/:id', getPostDetails);
+router.delete('/:id', deletePost);
+router.patch('/:id', updatePost);
 
 export default router;
