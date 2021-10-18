1231baec62a593dabcec219d1fad003c4ff87866
lesiukv <lesiyk47@gmail.com>
2021-10-18 14:50:50 +0300

    Trying to count number of topic values

--- controllers/topics.js
+++ controllers/topics.js
@@ -2,25 +2,32 @@ import postMessage from "../models/postMessage.js";
 
 export const getTopics = async (req, res) => {
   try {
+    const { topic } = req.params;
     const postMessages = await postMessage.find();
-    const arrayOfTopics = Object.keys(postMessages[0].toJSON());
     let uniqueValuesOfTopics = {};
     let newUniqueValuesOfTopics = {};
 
-    arrayOfTopics.forEach((topic) => {
-      uniqueValuesOfTopics[topic] = [
-        ...new Set(postMessages.map((post) => post[topic])),
-      ];
-      newUniqueValuesOfTopics[topic] = [].concat(
-        ...uniqueValuesOfTopics[topic]
-      );
-      newUniqueValuesOfTopics[topic] = newUniqueValuesOfTopics[topic].filter(
-        (element) => element != ""
-      );
-    });
+    uniqueValuesOfTopics[topic] = [
+      ...new Set(postMessages.map((post) => post[topic])),
+    ];
+
+    newUniqueValuesOfTopics[topic] = [].concat(...uniqueValuesOfTopics[topic]);
+    newUniqueValuesOfTopics[topic] = newUniqueValuesOfTopics[topic].filter(
+      (element) => element != ""
+    );
 
     res.status(200).json(newUniqueValuesOfTopics);
   } catch (error) {
     res.status(404).json(`${error}`);
   }
 };
+
+export const getTopicsNumber = async (req, res) => {
+  try {
+    const topic = req.body;
+    
+
+  } catch (error) {
+    res.json(`${error}`);
+  }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -7,17 +7,18 @@ import {
   updatePost,
 } from "../controllers/posts.js";
 import { postPages } from "../controllers/pages.js";
-import { getTopics } from "../controllers/topics.js";
+import { getTopics, getTopicsNumber } from "../controllers/topics.js";
 import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
-router.get("/topics", getTopics);
+router.get("/topics/:topic", getTopics);
 router.post("/", createPost);
 router.post("/uploads", upload.array("page"), postPages);
 router.get("/:id", getPostDetails);
 router.delete("/:id", deletePost);
+router.get("/topics/amount", getTopicsNumber);
 router.patch("/:id", updatePost);
 
 export default router;
