92918d014e86aa3d1050bb7b8e11b3520d045eac
lesiukv <lesiyk47@gmail.com>
2021-10-25 18:45:09 +0300

    remove gitignore files

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,4 +1,4 @@
-import postMessage from "../models/postMessage";
+import postMessage from "../models/postMessage.js";
 
 export const getComments = async (req, res) => {
   try {
@@ -21,11 +21,56 @@ export const getComment = async (req, res) => {
   }
 };
 
+export const addComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    let post = await postMessage.findById(id);
+    post.comments.push(req.body);
+    await postMessage.findByIdAndUpdate(id, post);
+    res.status(200).json(post);
+  } catch (error) {
+    res.json(`${error}`);
+  }
+};
+
 export const deleteComments = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const 
-    } catch (error) {
-        res.json(`${error}`);
+  try {
+    const { id } = req.params;
+    let post = await postMessage.findById(id);
+
+    for (let i = 0; i < post.comments.length; i++) {
+      post.comments.id(post.comments[i]._id).remove();
     }
+
+    await postMessage.findByIdAndUpdate(id, post);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.json(`${error}`);
+  }
+};
+
+export const deleteComment = async (req, res) => {
+  try {
+    const { id, commentId } = req.params;
+    let post = await postMessage.findById(id);
+    await postMessage.findByIdAndUpdate(
+      id,
+      post.comments.filter((comment) => comment._id != commentId)
+    );
+  } catch (error) {
+    res.json(`${error}`);
+  }
+};
+
+export const updateComment = async (req, res) => {
+  try {
+    const { id, commentId} = req.params;
+    const updatedComment = req.body;
+    let post = await postMessage.findById(id);
+    post.comments = updatedComment;
+    await postMessage.findByIdAndUpdate(id, post);
+  } catch (error) {
+    res.json(`${error}`);
+  }
 }
