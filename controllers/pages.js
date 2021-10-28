95e9cadd23f512cc27aeafc87ebafa21d9b273b7
lesiukv <lesiyk47@gmail.com>
2021-10-28 18:30:13 +0300

    Get Pages request

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,7 +1,18 @@
+import postMessage from "../models/postMessage.js";
+
 export const postPages = (req, res) => {
   try {
-    console.log('Page posted');
+    res.status(200).json("Page Uploaded")
   } catch (error) {
-    console.log(error);
+    console.log(`${error}`);
   }
 };
+
+export const getPages = (req, res) => {
+  try {
+    const { pages } = postMessage.findById(req.params);
+    res.status(200).json(pages);
+  } catch (error) {
+    console.log(`${error}`);
+  }
+}
--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { postPages } from "../controllers/pages.js";
+import { getPages, postPages } from "../controllers/pages.js";
 import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
+router.get("/:postId", getPages);
 router.post("/", upload.array("page"), postPages);
 
 export default router;
